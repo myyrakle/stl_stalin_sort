ee43845d614639826c59f04b2ef570b289106ecc
myyrakle <sssang97@naver.com>
2019-07-13 09:53:17 +0900

    Update stalin_sort.h

--- stalin_sort.h
+++ stalin_sort.h
@@ -16,7 +16,11 @@ namespace
 	
 		while(iter!=con.end())
 			if(*before > *iter)
+			{
 				iter = con.erase(iter);
+				before = iter;
+				before--;
+			}
 			else
 				++before, ++iter;
 	}
@@ -37,7 +41,15 @@ void stalin_sort(std::vector<T>& vec)
 template <class T>
 void stalin_sort(std::forward_list<T>& li)
 {
-	__impl_stalin_sort(li);
+	auto iter = li.begin();
+	auto after = iter;
+	after++;
+	
+	while(after!=li.end())
+		if(*iter > *after)
+			after = li.erase_after(iter);
+		else
+			++after, ++iter;
 }
 
 template <class T>
